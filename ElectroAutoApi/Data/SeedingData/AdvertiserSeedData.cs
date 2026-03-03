27587ffc291c16e4f4a55a9758eca5ea759f2cc7
RoeiBoot <67387670+Yorboot@users.noreply.github.com>
2026-03-03 16:56:18 +0100

    Delete ElectroAutoApi directory

--- ElectroAutoApi/Data/SeedingData/AdvertiserSeedData.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ElectroAutoApi.Data.SeedingData;
-
-public class AdvertiserSeedData
-{
-    public List<string> FirstNames { get; set; } = new();
-    public List<string> LastNames { get; set; } = new();
-    public List<string> EmailDomains { get; set; } = new();
-    public List<string> CommonPasswords { get; set; } = new();
-}
